58146d176bbf5dfc1b21fd5f3fc11a80cbff4c8a
Vidura Priyadarshana <vidura200427@gmail.com>
2025-06-08 23:16:37 +0530

    Search Debounce Complete

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from "react";
 import Search from "./components/Search";
 import Spinner from "./components/Spinner";
 import MovieCard from "./components/MovieCard";
+import { useDebounce } from "react-use";
 
 const API_BASE_URL = "https://api.themoviedb.org/3";
 const API_KEY = import.meta.env.VITE_TMDB_API_KEY;
@@ -14,11 +15,16 @@ const API_OPTIONS = {
   },
 };
 
+
+
 const App = () => {
   const [searchTerm, setSearchTerm] = useState("");
   const [errorMessage, setErrorMessage] = useState("");
   const [movieList, setMovieList] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
+  const [debouncedSearchTerm, setDebouncedSearchTerm] = useState('');
+
+  useDebounce( () => setDebouncedSearchTerm(searchTerm) , 500 , [searchTerm]);
 
   const fetchMovies = async (query = "") => {
     setIsLoading(true);
@@ -52,8 +58,8 @@ const App = () => {
   };
 
   useEffect(() => {
-    fetchMovies(searchTerm);
-  }, [searchTerm]);
+    fetchMovies(debouncedSearchTerm);
+  }, [debouncedSearchTerm]);
 
   return (
     <main>
