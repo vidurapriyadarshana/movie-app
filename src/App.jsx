188b12d2326e62114dcca7aed945c8f06a515fac
Vidura Priyadarshana <vidura200427@gmail.com>
2025-06-08 22:55:53 +0530

    Search IMPL Complete

--- src/App.jsx
+++ src/App.jsx
@@ -15,18 +15,19 @@ const API_OPTIONS = {
 };
 
 const App = () => {
-
   const [searchTerm, setSearchTerm] = useState("");
   const [errorMessage, setErrorMessage] = useState("");
   const [movieList, setMovieList] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
 
-  const fetchMovies = async () => {
+  const fetchMovies = async (query = "") => {
     setIsLoading(true);
     setErrorMessage("");
 
     try {
-      const endpoint = `${API_BASE_URL}/discover/movie?sort_by=popularity.desc`;
+      const endpoint = query
+        ? `${API_BASE_URL}/search/movie?query=${encodeURIComponent(query)}`
+        : `${API_BASE_URL}/discover/movie?sort_by=popularity.desc`;
       const response = await fetch(endpoint, API_OPTIONS);
 
       if (!response.ok) {
@@ -51,8 +52,8 @@ const App = () => {
   };
 
   useEffect(() => {
-    fetchMovies();
-  }, []);
+    fetchMovies(searchTerm);
+  }, [searchTerm]);
 
   return (
     <main>
@@ -85,7 +86,6 @@ const App = () => {
       </div>
     </main>
   );
-
 };
 
 export default App;
