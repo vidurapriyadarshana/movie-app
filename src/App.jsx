9f2f499c57722fb351ff3797f15c2a0081f7755c
Vidura Priyadarshana <vidura200427@gmail.com>
2025-06-08 16:49:35 +0530

    Hero Complete

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
+import Search from "./components/Search";
 
 const App = () => {
+
+  const [searchTerm , setSearchTerm] = useState('');
+
   return (
     <main>
       <div className="pattern" />
       <div className="wrapper">
         <header>
-          <h1>Find <span className="text-gradient">Movies</span> You'll Enjoy Without the Hassle</h1>
+          <img src="./hero.png" alt="Hero Banner" />
+          <h1>
+            Find <span className="text-gradient">Movies</span> You'll Enjoy
+            Without the Hassle
+          </h1>
         </header>
+
+        <Search searchTerm={searchTerm} setSearchTerm={setSearchTerm} />
       </div>
     </main>
   );
