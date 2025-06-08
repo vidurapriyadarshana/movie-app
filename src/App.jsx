62d581a3e1811caad8336071f466e22e5be72744
Vidura Priyadarshana <vidura200427@gmail.com>
2025-06-08 16:22:28 +0530

    Initial Commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <main>
+      <div className="pattern" />
+      <div className="wrapper">
+        <header>
+          <h1>Find <span className="text-gradient">Movies</span> You'll Enjoy Without the Hassle</h1>
+        </header>
+      </div>
+    </main>
+  );
+};
+
+export default App;
