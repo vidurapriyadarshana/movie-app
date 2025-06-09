b4589fff7d72fc29a9910a43efc0712bd23b0544
Vidura Priyadarshana <vidura200427@gmail.com>
2025-06-10 00:12:50 +0530

    appwrite data save done

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Search from "./components/Search";
 import Spinner from "./components/Spinner";
 import MovieCard from "./components/MovieCard";
 import { useDebounce } from "react-use";
+import { updateSearchCount } from "./appwrite";
 
 const API_BASE_URL = "https://api.themoviedb.org/3";
 const API_KEY = import.meta.env.VITE_TMDB_API_KEY;
@@ -49,6 +50,11 @@ const App = () => {
       }
 
       setMovieList(data.results || []);
+
+      if (query && data.results.length > 0) {
+        await updateSearchCount(query , data.results[0]);
+      }
+
     } catch (error) {
       console.log(`Error while fethcing movies: ${error}`);
       setErrorMessage("Error while fetching movies plese try again later.");
